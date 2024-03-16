4bfb8022dcea496da336205db07b944f28106326
Oleksandr Shuliar <shoulyar@live.com>
2024-03-16 09:20:24 +0100

    hw2task1

---
+++ HW2Task1/Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2Task1
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public Character(string name, int power)
+        {
+            Name = name;
+            if (power <= 0)
+            {
+                throw new ArgumentException("Power should be more than 0: ");
+            }
+            else
+            {
+                Power = power;
+            }
+        }
+
+        public Character CallCharacter(Character character)
+        {
+            return character;
+        }
+        public void Pull(Plant plant)
+        {
+            plant.ChangeWeight(this.Power);
+        }
+    }
+}
---
+++ HW2Task1/Plant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2Task1
+{
+    public class Plant
+    {
+        public string Name { get; set; }
+        public int Weight { get; private set; }
+
+        public Plant(string name, int weight)
+        {
+            Name = name;
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Weight should be more than 0:");
+            }
+            else
+            {
+                Weight = weight;
+            }
+        }
+        public void ChangeWeight(int weight)
+        {
+            Weight -= weight;
+        }
+    }
+}
